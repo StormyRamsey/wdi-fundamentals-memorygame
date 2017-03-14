13b91430e3ff2d5aecf5aa8666d0f56086e35bf6
Stormy K Ramsey <stormy.ramsey@gmail.com>
2017-03-14 11:42:56 -0400

    Memory Game changes from Unit 1 - 9
    Hard upload due to terminal error message.

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+if (cardTwo === cardFour) {
+	alert ("You found a match!" );
+} else if (cardOne === cardThree) {
+	alert ("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert ("Sorry try again.");
+} else if (cardThree === cardFour) {
+	alert ("Sorry try again.");
+}
